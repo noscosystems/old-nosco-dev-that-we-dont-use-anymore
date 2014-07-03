c26de2c7847ac9a42f05abcc3dc90903a6e82ffc
Ivaylo Raykov <ivailo_raikov@hotmail.com>
2014-07-03 16:55:25 +0300

    This is the admin forms to edit email,username and company

---
+++ application/views/admin/view.php
@@ -0,0 +1,62 @@
+<html>
+  <head>
+  <meta charset="utf8">
+    <link rel="stylesheet" type="text/css" href="/projects/noscobg/public_html/assets/3baca50d/css/bootstrap.min.css" media="all">
+   <link rel="stylesheet" type="text/css" href="/projects/nosco-dev/application/themes/classic/assets/css/main.css" media="all">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/projects/noscobg/public_html/assets/3baca50d/js/bootstrap.min.js"></script>
+  </head>
+    <body>
+      <div class="panel panel-primary text-center"><h1>Administrator Panel</h1><br /><h3>Hello <!--<?php echo $user->user_name ?>--></h3></div>
+     
+       <!-- <div class="form-group">
+          <<input type="text" class="form-control pull-right" placeholder="Search">
+        </div>-
+          <button type="submit" class="btn btn-default">Submit</button>-->
+<ul class="nav nav-tabs nav-justified" role="tablist">
+  <li class="active"><a href="#"  >Company</a></li>
+
+
+  </script>
+  <li class="active1"><a href="viewEmail.php" >Email</a></li>
+  <li><a href="viewUsername.php">Username</a></li>
+</ul>
+      </form><br/ >
+     
+      <div class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal --><br/><br/ >
+<br /><button type="button" class="btn btn-default btn-sm pull-right" value="Edit">
+ New Company
+</button><br />
+
+<table class="table table-striped">
+  <tr>
+    <td class="active">
+    <button type="button" class="btn btn-danger pull-right">Delete</button>
+      <button type="button" class="btn btn-info pull-right">Edit</button>
+      
+    </td>
+  </tr><br /><br/>
+<tr class=>
+  <td class="info">
+  <button type="button" class="btn btn-danger pull-right">Delete</button>
+    <button type="button" class="btn btn-info pull-right">Edit</button>
+    
+  </td>
+</tr>
+</table>       
+</div>
+    </body>
+</html>
---
+++ application/views/adminxx/AdminEdit.php
@@ -0,0 +1,73 @@
+<html>
+  <head>
+  <meta charset="utf8">
+    <link rel="stylesheet" type="text/css" href="/projects/noscobg/public_html/assets/3baca50d/css/bootstrap.min.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/projects/nosco-dev/application/themes/classic/assets/css/main.css" media="all">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/projects/noscobg/public_html/assets/3baca50d/js/bootstrap.min.js"></script>
+  </head>
+    <body>
+     <div class="panel panel-primary text-center">
+        <h1>Administrator Panel</h1><br /><h3>Hello <!--<?php echo $user->user_name ?>--></h3>
+     </div>
+     <!-- <form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+        <input type="text" class="form-control pull-right" placeholder="Search">
+        </div>
+          <button type="submit" class="btn btn-default">Submit</button>-->
+         <!-- <ul class="nav nav-tabs nav-justified" role="tablist">
+            <li class="active"><a href="#"  >Company</a></li>
+
+
+            </script>
+          <li class="active1"><a href="#" >Email</a></li>
+          <li><a href="#">Username</a></li>
+            </ul>
+        </div>
+      </form>
+    </div>
+
+  
+  <form class="form-horizontal" role="form" >-->
+ <!-- action="<?php// echo $this->createUrl(array('/admin/create')) ?>" mothod="post" it must be put at form with class form-horiontal-->
+  <br/>
+	<div class="form-group form-group-sm">   
+    <div class="col-sm-2 control-label pull-left" for="formGroupInputSmall"><br/>User name <!--<?php echo $user->user_name ?>--> </div>
+    <div class="col-sm-10">
+
+      <input class="form-control pull-left" type="text" id="formGroupInputSmall" placeholder="">
+      <span class="help-block">Please enter the new User name.</span>
+    </div>
+  </div>
+  <div class="form-group form-group-sm"> 
+   <div class="col-sm-2 control-label pull-left" for="formGroupInputSmall"><br/><br/>Password</div>
+    <div class="col-sm-10"><br/>
+      <input class="form-control pull-left" type="text" id="formGroupInputSmall" placeholder="">
+      <span class="help-block">Please enter the new password.</span>
+    </div>
+  </div>
+   <div class="form-group form-group-sm"> 
+   <div class="col-sm-2 control-label pull-left" for="formGroupInputSmall"><br/><br/>Repeat password</div>
+    <div class="col-sm-10"><br/>
+      <input class="form-control pull-left" type="text" id="formGroupInputSmall" placeholder="">
+      <span class="help-block">Please repeat the new password.</span>
+    </div>
+  </div>
+  <div class="form-group form-group-sm">   
+    <div class="col-sm-2 control-label pull-left" for="formGroupInputSmall"><br/><br/>Company <!--<?php echo $user->Company ?>--></div>
+    <div class="col-sm-10"><br/>
+      <input class="form-control pull-left" type="text" id="formGroupInputSmall" placeholder="">
+      <span class="help-block">Please enter the new Company.</span>
+    </div>
+  </div>
+  <div class="form-group form-group-sm"> 
+   <div class="col-sm-2 control-label pull-left" for="formGroupInputSmall"><br/><br/>Email <!--<?php echo $user->Email ?>--></div>
+    <div class="col-sm-10"><br/>
+      <input class="form-control pull-left" type="text" id="formGroupInputSmall" placeholder="">
+      <span class="help-block">Please enter the new email address.</span>
+    </div>
+  </div>
+</form>
+ <br /><input type="submit" class="btn btn-primary btn pull-right" value="Save"></input>
+	</body>
+</html>
